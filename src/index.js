8dd5c4073cf66ac8ab1c06de75e63c08654133e4
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-03-29 15:47:18 +0200

    200322 after components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//getElementById is that it only allows to select an element by its id. 
+//The querySelector method can be used in other situations as well, 
+//such as when selecting by element name, nesting, or class name.
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
