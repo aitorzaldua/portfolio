7b932c61c234566bed06affbe2dea90b4d339aff
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-03-31 18:36:06 +0200

    310322 services before media queries

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,108 @@
 import React from 'react';
 import './experience.css';
+import {BsPatchCheckFill} from 'react-icons/bs';
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__solidity'>
+          <h3>EVM Blockchains</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+              <h4>Ethereum</h4>
+              <small className='text-light'>Experienced</small>
+              </div>             
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Polygon</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Arbitrum</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Avalanche</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Cronos</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+          </div>
+        </div>
+
+        <div className='experience__rust'>
+          <h3>Non-EVM Blockchains</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Solana</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Substrate</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Near</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Holochain</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Terra</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,105 @@
 import React from 'react';
 import './services.css';
+import {BiCheck} from 'react-icons/bi';
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services__container'>
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Frontend & UI Design</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>First description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Second description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Thirth description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Fourth description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Fiveth description of Service</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>EVM Services</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>First description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Second description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Thirth description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Fourth description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Fiveth description of Service</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Non-EVM Services</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>First description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Second description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Thirth description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Fourth description of Service</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Fiveth description of Service</p>
+            </li>
+          </ul>
+        </article>
+
+
+      </div>
+
+
+
+    </section>
   )
 }
 
