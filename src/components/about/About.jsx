97cdaa5a0139347d8dc6a8ca8b6842d503307942
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-03-31 15:02:03 +0200

    310322 after about_me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/me-about.jpg';
+import {FaAward} from 'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc';
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt="This is me" />
+          </div>
+        </div>
+
+
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>24+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>100+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>50+ Completed </small>
+            </article>        
+          </div>
+
+          <p>
+            Working for more than 24 years for the biggest techs in the world,
+            for the best companies, biggest projects and also acomplished the
+            challenges that I try because I am the best engineer in the entire world.
+            </p>
+            <a href='#contact' className='btn btn-primary'>Let´s talk</a>
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
