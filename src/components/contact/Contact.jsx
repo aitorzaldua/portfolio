268bd40b1426dfc590bc4c14ada15d77f2d5c279
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-04-02 09:25:51 +0200

    020322 finished before build

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,26 +14,35 @@ const Contact = () => {
       <div className='container contact__container'>
         <div className='contact__options'>
           <article className='contact__option'>
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h5>aitor.zaldua@draftdigital.org</h5>
+            {/* <h5>aitor.zaldua@draftdigital.org</h5> */}
             <a href='mailto:aitor.zaldua@draftdigital.org' target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
-            <h5>Click to send a message</h5>
-            <a href='https://wa.me/+34609278125' target="_blank" rel="noreferrer">Send a message</a>
+            {/* <h5>Click to send a message</h5> */}
+            {/* <a href='https://wa.me/+34609278125' target="_blank" rel="noreferrer">Send a message</a> */}
+            <a href='https://api.whatsapp.com/send?phone=+34609278125' target="_blank" rel="noreferrer">Send a message</a>
           </article>
 
           <article className='contact__option'>
-            <BsLinkedin/>
+            <BsLinkedin className='contact__option-icon' />
             <h4>Linkedin</h4>
-            <h5>Click to access to my profile</h5>
+            {/* <h5>Click to access to my profile</h5> */}
             <a href='https://www.linkedin.com/in/aitor-zaldua/' target="_blank" rel="noreferrer">Send a message</a>
           </article>
         </div>
+
+        <form action="">
+          <input type='text' name='name' placeholder='Your full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required/>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+
+        </form>
       </div>
     </section>  
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import './footer.css';
 
+import {BsLinkedin} from 'react-icons/bs';
+import {IoLogoTwitter} from 'react-icons/io';
+import {FiInstagram} from 'react-icons/fi';
+import {AiFillHeart} from 'react-icons/ai';
+
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <section id='footer' className='footer'>
+      <a href='#' className='footer__logo'>Aitor Zaldua</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About Me</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#blog'>Blog</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+    <div className='footer__socials'>
+      <a href='https://linkedin.com'><BsLinkedin/></a>
+      <a href='https://twitter.com'><IoLogoTwitter/></a>
+      <a href='jttps://instagram.com'><FiInstagram/></a>
+    </div>
+
+    <div className='footer__copyright'>
+      <small>&copy; Made with <AiFillHeart/> by MG</small>
+    </div>
+
+
+    </section>
   )
 }
 
