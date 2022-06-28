1ea30a787f32ff967db85b1ad12f477d63c9e0f4
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-06-28 19:46:26 +0200

    add crypto transfers

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import "./portfolio.css";
 import IMG1 from "../../assets/netflix.png";
 import IMG2 from "../../assets/airbnb.png";
 import IMG3 from "../../assets/twitter.png";
-import IMG4 from "../../assets/cryptocard.png";
+import IMG4 from "../../assets/cryptoTranfers.png";
 import IMG5 from "../../assets/coinmarket.png";
 import IMG6 from "../../assets/draftdigital.png";
 
